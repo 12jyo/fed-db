3c183d934a0f2495b01a612508dbd815b76d4e09
Ritwik Mukherjee <mritwik369@gmail.com>
2021-12-29 14:31:12 +0530

    feat: validation added

--- api/router.js
+++ api/router.js
@@ -7,8 +7,8 @@ module.exports = controller => {
     router.get("/get-all-collections", controller.getAllCollections);
     // router.get("/get-collection", controller.getCollection);
     router.get("/:collectionName/get-all-documents", controller.getAllDocuments);
-    router.post("/get-document-by-id", controller.getDocumentById);
-    router.post("/edit-document", controller.editDocument);
+    router.post("/get-document-by-id", validator.getDocumentById, controller.getDocumentById);
+    router.post("/edit-document", validator.editDocument, controller.editDocument);
     // router.post("/query-documents", controller.queryDocuments);
 
     router.post("/document", validator.createDocument, controller.createDocument);
--- api/validator.js
+++ api/validator.js
@@ -18,3 +18,42 @@ module.exports.createDocument = async (req, res, next) => {
 
     next();
 }
+
+module.exports.getDocumentById = async (req, res, next) => {
+    const schema = joi.object({
+        collectionName: joi.string().regex(new RegExp(/^[a-zA-Z]+$/)).required(),
+        id: joi.string().regex(new RegExp(/^[0-9A-F]{8}-[0-9A-F]{4}-4[0-9A-F]{3}-[89AB][0-9A-F]{3}-[0-9A-F]{12}$/)).required()
+    });
+
+    try {
+        await schema.validateAsync(req.body);
+    } catch (error) {
+        return res.json({
+            error: "validation error",
+            stackTrace: error,
+            success: false
+        })
+    }
+
+    next();
+};
+
+module.exports.editDocument = async (req, res, next) => {
+    const schema = joi.object({
+        collectionName: joi.string().regex(new RegExp(/^[a-zA-Z]+$/)).required(),
+        id: joi.string().regex(new RegExp(/^[0-9A-F]{8}-[0-9A-F]{4}-4[0-9A-F]{3}-[89AB][0-9A-F]{3}-[0-9A-F]{12}$/)).required(),
+        modifiedData: joi.object().required()
+    });
+
+    try {
+        await schema.validateAsync(req.body);
+    } catch (error) {
+        return res.json({
+            error: "validation error",
+            stackTrace: error,
+            success: false
+        })
+    }
+
+    next();
+};
